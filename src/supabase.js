46f2be240ba72eeef13e815233d2ec7ffeb2117d
Anatolii Sydorenko <anatolii@MacBookPro.lan>
2026-05-31 14:30:32 +0100

    use legacy anon key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  'https://ypjogxdrcargvufgzchn.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlwam9neGRyY2FyZ3Z1Zmd6Y2huIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMDQwNjgsImV4cCI6MjA5NTU4MDA2OH0.1IRvCi-RhmrnpA7homFz4bQ3QxxHB440b80dQVq0skk'
 )
